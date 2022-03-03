0ca9885c457ae1aba0761a8288aaaae4e36ce46b
Ygor Pereira de Sá <80071063+ygorxds@users.noreply.github.com>
2022-03-03 17:28:43 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const moongoose = require('mongoose');
 
 try{
   
-  moongoose.connect('mongodb://ygor:senha123@127.0.0.1', { useNewUrlParser: true, useUnifiedTopology: true });
+  moongoose.connect('mongodb://<nomedobanco>:<senhadobanco>@127.0.0.1', { useNewUrlParser: true, useUnifiedTopology: true });
   moongoose.Promise = global.Promise;
 }catch(err) { 
   console.log (err);
